19a9101e7edcd4f82c75f70ea28fa445d5ca2a43
Dan Gillette <daniel.gillette87@gmail.com>
2020-08-30 14:29:48 -0700

    App has required functionality, pushing to GitHub and then I will deploy on Heroku

---
+++ Develop/server.js
@@ -0,0 +1,134 @@
+const express = require("express");
+const bodyParser = require('body-parser')
+const fs = require("fs")
+const app = express()
+const path = require('path');
+const PORT = 3000
+
+
+
+
+app.use(express.static("public"))
+app.use(express.json());
+
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+
+let notes = require('./db/db');
+
+
+
+
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+  });
+  
+  app.get('/notes', function (req, res) {
+    res.sendFile(path.join(__dirname, 'public/notes.html'));
+  });
+
+  
+  
+  
+  app.get('/api/notes', function (req, res) {
+    return res.json(notes);
+  });
+  
+  
+  app.get('/api/notes/:note', function (req, res) {
+    let chosen = req.params.note;
+  
+    for (let i = 0; i < notes.length; i++) {
+      if (chosen === notes[i].id) {
+        return res.json(notes[i]);
+      }
+    }
+  
+    return res.json(false);
+  });
+  
+
+  app.post('/api/notes', function (req, res) {
+
+
+    fs.readFile("./db/db.json", "utf8", function(error, data) {
+
+      if (error) {
+        return console.log(error);
+      }
+
+      let tempArray = JSON.parse(data)
+
+      let newNote = req.body;
+
+      newNote.id = newNote.title.replace(/\s+/g, '').toLowerCase();
+
+      notes.push(newNote)
+  
+    
+      tempArray.push(newNote);
+
+      newArray = JSON.stringify(tempArray)
+
+      fs.writeFile ("./db/db.json", newArray, "utf8", function(err){
+        if (err){
+          throw err;
+        }
+      })
+
+      res.json(false);
+    
+    })
+    
+  });
+
+
+  app.delete('/api/notes/:id', function (req, res) {
+
+    fs.readFile("./db/db.json", "utf8", function(error, data) {
+
+      if (error) {
+        return console.log(error);
+      }
+
+      let tempArray = JSON.parse(data)
+
+      const deletedNote = req.params.id;
+
+      const index = notes.map(function(e) {return e.id; }).indexOf(deletedNote);
+
+      tempArray.splice(index, 1)
+
+      notes.splice(index, 1)
+
+      
+
+      newArray = JSON.stringify(tempArray)
+
+      fs.writeFile ("./db/db.json", newArray, "utf8", function(err){
+        if (err){
+          throw err;
+        }
+      })
+
+      for (let i = 0; i < notes.length; i++) {
+      if (deletedNote === notes[i].id) {
+        return res.json(notes[i]);
+
+      }
+
+    }
+  
+      res.json(true);
+    
+    })
+  
+});
+
+
+  app.listen(PORT, function () {
+    console.log(`App listening on PORT: ${PORT}`);
+  });
+  
+  
--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-# notetaker
+# ReadMe for Note Taker 
+
+  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+  # Table of Contents: 
+
+  [Description](#description)  
+  [License](#license)  
+  [Installation](#installation)  
+  [Usage](#usage)  
+  [License](#license)  
+  [Contribution](#contribution)  
+  [Testing](#testing)  
+  [Questions](#questions)  
+  
+
+
+  ## Description
+    
+  Keep Track of your To Do list with this easy-to-use app!
+
+  ## License
+    
+  MIT
+
+  ## Installation
+    
+  This app lives in HeroKu, but to install locally clone the GitHub repo and run NPM install to obtain the node modules.
+
+  ## Usage
+    
+  Click the "Get Started" button.  On the notes page, type in your note title, note description and click the "Save" icon.  You can click on an agenda item to display and read it.  To delete a note, click the "Trash" icon.
+
+  ## Contribution 
+    
+  Feel free to pull the GitHub repo and make whatever changes you see fit.
+    
+  ## Testing
+    
+  Run the app, your terminal should log any errors
+    
+  ## Questions
+
+  For questions you may email me at gillettedan@ymail.com.
+
+  GitHub Profile Link: https://github.com/DanGillette25
+    
+  
